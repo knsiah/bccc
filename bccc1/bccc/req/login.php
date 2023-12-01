ff6aa8fd04402a33dfc2e067ca2f205bc809d14f
knsiah <123516586+knsiah@users.noreply.github.com>
2023-12-01 07:13:03 +0000

    login.php

--- bccc1/bccc/req/login.php
+++ bccc1/bccc/req/login.php
@@ -27,7 +27,6 @@ if (
         header("Location: ../login.php?error=$em");
         exit;
     } else {
-
         if ($role == '1') {
             $result = mysqli_query($db, "SELECT * FROM admin WHERE username = '$uname'");
             $role = "admin";
@@ -39,67 +38,52 @@ if (
             $role = "Student";
         }
 
-
-
         if (mysqli_num_rows($result) > 0) {
             // output data of each row
             if ($row = mysqli_fetch_assoc($result)) {
-
-                
                 $username = $row['username'];
                 $password = $row['password'];
 
-                 // Assuming you fetch 'fname' from the database
-                
-                if ($username === $uname) {
-                 if (password_verify($pass, $password)) {
-                   
+                if ($username === $uname && password_verify($pass, $password)) {
                     $_SESSION['role'] = $role;
                     if ($role == 'Admin') {
-                        $id = $user['admin_id'];
+                        $id = $row['admin_id'];
                         $_SESSION['admin_id'] = $id;
                         header("Location: ../admin/index.php");
                         exit;
-                    }}else if ($role == 'Student') {
-                        $id = $user['student_id'];
+                    } else if ($role == 'Student') {
+                        $id = $row['student_id'];
                         $_SESSION['student_id'] = $id;
                         header("Location: ../Student/index.php");
                         exit;
-                    }else if ($role == 'Registrar Office') {
-                        $id = $user['r_user_id'];
+                    } else if ($role == 'Registrar Office') {
+                        $id = $row['r_user_id'];
                         $_SESSION['r_user_id'] = $id;
                         header("Location: ../RegistrarOffice/index.php");
                         exit;
-                    }else if($role == 'Teacher'){
-                    	$id = $user['teacher_id'];
+                    } else if ($role == 'Teacher') {
+                        $id = $row['teacher_id'];
                         $_SESSION['teacher_id'] = $id;
                         header("Location: ../Teacher/index.php");
                         exit;
-                    }else {
-                    	$em  = "Incorrect Username or Password";
-				        header("Location: ../login.php?error=$em");
-				        exit;
+                    } else {
+                        $em = "Incorrect Username or Password";
                     }
-				    
-            	}else {
-		        	$em  = "Incorrect Username or Password";
-				    header("Location: ../login.php?error=$em");
-				    exit;
-		        }
-            }else {
-	        	$em  = "Incorrect Username or Password";
-			    header("Location: ../login.php?error=$em");
-			    exit;
-	        }
-        }else {
-        	$em  = "Incorrect Username or Password";
-		    header("Location: ../login.php?error=$em");
-		    exit;
+                } else {
+                    // Incorrect password or username
+                    $em = "Incorrect Username or Password";
+                }
+            } else {
+                // User not found
+                $em = "User not found";
+            }
+        } else {
+            $em = "User not found";
         }
-	}
-
-
-}else{
-	header("Location: ../login.php");
-	exit;
+        header("Location: ../login.php?error=$em");
+        exit;
+    }
+} else {
+    header("Location: ../login.php");
+    exit;
 }
